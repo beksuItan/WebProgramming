9dda52d4fa48b4ec2314feccbe7bd7234b0967c9
kbeksultan <bkakpanbay@gmail.com>
2020-10-22 18:40:42 +0600

    upd

--- lab8/calculator/calc.js
+++ lab8/calculator/calc.js
@@ -1,29 +1,29 @@
-function calc(operation){
-    switch(operation){
-        case '+':
-            result = first + second;
-            break;
-        case '-':
-            result = first - second;
-            break;
-        case '*':
-            result = first * second;
-            break;
-        case '/':
-            result = first / second;
-            break;
-        case '%':
-            result = first % second;
-            break;
-        default:
-            alert('You need enter value');
-    }
+function addition(first, second){
+    result = first + second;
+    alert(result);
+}
 
+function substraction(first, second){
+    result = first - second;
+    alert(result);
+}
+
+function multiplication(first, second){
+    result = first * second;
+    alert(result);
+}
+
+function division(first, second){
+    result = first / second;
     alert(result);
 }
 
 let first = +prompt("Enter first number");
 let second = +prompt("Enter second number");
-let operation = prompt("Enter operation");
 
-calc(operation, first, second);
+addition(first, second);
+substraction(first, second);
+multiplication(first, second);
+division(first, second);
+
+
--- lab8/palindrome/palindrome.js
+++ lab8/palindrome/palindrome.js
@@ -10,9 +10,18 @@
 // }
 
 let isPalindrome = (word) => {
-    reversed = word.split("").reverse().join("");
+    // reversed = word.split("").reverse().join("");
+    // (word == reversed) ? alert("Palindrome") : alert("Not palindrome");
 
-    (word == reversed) ? alert("Palindrome") : alert("Not palindrome");
+    let isPal = true;
+    for(let i = 0; i <= word.length;i++){
+        if(word[i] !== word[word.length - 1 - i]){
+            isPal = false;
+        }
+    }
+
+    (isPal) ? alert("Palindrome"): alert("Not palindrome");
+
 };
 
 let word = prompt("Enter word: ");
